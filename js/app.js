6292eb0a85321f8a5b3fb12c5d3b6c6f759c33f5
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2023-12-19 17:59:28 +0300

    display post text

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 let text = "";
 const textInputNode = document.querySelector(".js-post__text");
 const postButtonNode = document.querySelector(".js-post__publication-button");
+const postsNode = document.querySelector(".js-posts__item");
 
 console.log(textInputNode);
 console.log(postButtonNode);
 
 postButtonNode.addEventListener("click", function () {
   postText = textInputNode.value;
+  postsNode.innerText = postText;
   console.log(postText);
 });
